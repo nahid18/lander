f986cf0375a6d6b3b099864805c564191bdbddb8
Sakshi-00 <88303597+Sakshi-00@users.noreply.github.com>
2021-08-02 22:23:48 +0530

    Delete info.c

--- info.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main(){
-    printf("Name : Sakshi Rawat\n");
-    printf("Email : sakshirawat3oct@gmail.com\n");
-    printf("Language : C\n");
-    printf("Biostack : Genomics\n");
-    printf("Slack Username :%cSakshi\n",64);
-    return 0;
-}
