0d74c95073217273a3ff5bd11491c53daadb7275
Sakshi-00 <88303597+Sakshi-00@users.noreply.github.com>
2021-08-02 22:08:07 +0530

    Add files via upload

---
+++ info.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    printf("Name : Sakshi Rawat\n");
+    printf("Email : sakshirawat3oct@gmail.com\n");
+    printf("Language : C\n");
+    printf("Biostack : Genomics\n");
+    printf("Slack Username :%cSakshi\n",64);
+    return 0;
+}
