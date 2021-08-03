6c96de560e306c511d80847b6caabc0ed239d4a2
Akaniro Ifunanya <71774308+Iffy27@users.noreply.github.com>
2021-08-03 10:13:37 +0100

    Add files via upload

---
+++ stage_0_Iffy.rb
@@ -0,0 +1,10 @@
+my_name="Akaniro Ifunanya"
+my_email="joiceiffy22@gmail.com"
+my_language="Ruby"
+my_biostack="Genomics and Data analysis"
+my_slackusername="@Iffy"
+puts my_name
+puts my_email
+puts my_language
+puts my_biostack
+puts my_slackusername
