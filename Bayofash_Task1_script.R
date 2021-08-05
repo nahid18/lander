d21f04b8fcf85a14fdcca082dc48d8b3a6c4a308
Ilemobayo Victor FASOGBON <fasogbonilemobayo@gmail.com>
2021-08-05 02:03:54 +0100

    Add files via upload

---
+++ Bayofash_Task1_script.R
@@ -0,0 +1,13 @@
+# this is my first R script, kudos to HackBio
+
+print("My Mame: Fasogbon Ilemobayo Victor") # my surname is Fasogbon
+
+print("My Email address: fasogbonilemobayo@gmail.com")
+
+print("The Language I used: R")
+
+print("My Biostack name: Genomics")
+
+print("My slack name: @Bayo")
+
+# I love team-lander
