6282b4f08eb2dd92410cfeb4d77ac6a872e9ecec
Modinat Abayomi <88114037+Modeenat@users.noreply.github.com>
2021-08-06 13:55:33 +0100

    Add files via upload

---
+++ stage_0_modinat.r
@@ -0,0 +1,11 @@
+slack <- "@Modinat"
+twitter <- "@Modinat"
+
+hamming <- sum(slack != twitter)
+
+cat("Name: Modinat Abayomi\n")
+cat("Email: modeenat@gmail.com\n")
+cat("Slack Username: @Modinat\n")
+cat("Biostack: Drug Discovery\n")
+cat("Twitter Handle: @Modinat\n")
+cat(paste("Hamming Distance:",hamming,"\n"))
