2869ecfeaf989460163bac93f1ce2b929ca03074
MahfuzRoby <72339237+MahfuzRoby@users.noreply.github.com>
2021-08-05 23:31:03 -0700

    stage_0_mahfuz

--- stage_0_mahfuz.py
+++ stage_0_mahfuz.py
@@ -1,19 +1,22 @@
 def hamming(slack,twitter):
-    result=0
-    if len(slack)!=len(twitter):
-        print("String are not equal")
-    else:
-        for x,(i,j) in enumerate(zip(slack,twitter)):
-            if i!=j:
-                print(f'char not math{i,j}in {x}')
-                result+=1
-    return result
+
+    distance = 0
+
+    L = len(slack)
+    for i in range(L):
+
+        if slack[i] != twitter[i]:
+            distance += 1
+
+    return distance
+
 name="Md. Mahfuzur Rahman Bhuyan"
 email="mdmahfuzurrahmanbhuyan@gmail.com"
 slackid="@Mahfuz"
-twitterid="@Mahfuz_"
+twitterid="@RobyRahman13"
 print('Name :',name)
 print('Email :',email)
 print('Slack ID:',slackid)
 print('Twitter ID:',twitterid)
 print("Hamming Distance between Slack and Twitter:",hamming(slackid,twitterid))
+
