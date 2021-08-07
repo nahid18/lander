8dbb168d274691f08f834c427b660e42bfdc4967
Ilemobayo Victor FASOGBON <fasogbonilemobayo@gmail.com>
2021-08-07 05:11:47 +0100

    Add files via upload

--- stage_0_Fasogbonbayo.R
+++ stage_0_Fasogbonbayo.R
@@ -1,17 +1,37 @@
-# this is my first R script, kudos to HackBio
+# Hello world,
 
-print("My Mame: Fasogbon Ilemobayo Victor") # my surname is Fasogbon
+cat("My Mame: Fasogbon Ilemobayo Victor") # my surname is Fasogbon
 
-print("My Email address: fasogbonilemobayo@gmail.com")
+cat("My Email address: fasogbonilemobayo@gmail.com")
 
-print("The Language I used: R")
+cat("My Biostack name: Genomics")
 
-print("My Biostack name: Genomics")
+cat("My slack username: @Fasogbonbayo")
 
-print("My slack username: @Fasogbonbayo")
+cat("My Twitter Handle: @Fasogbonfash")
 
-print("Twitter Handle: @Fasogbonbayo")
+# Calculating the hamming distance "My slack username: @Fasogbonbayo" and "My Twitter Handle: @Fasogbonfash"
 
-print("Hamming Distance: 0")
+string1 <- "Fasogbonbayo"
+string2 <- "Fasogbonfash"
+count <- 0 # counter
+
+# Gets the number of characters in a string 
+# Also gets if both are equal
+# nchar counts the number of characters in a string.
+min_length = min(nchar(string2), nchar(string2))
+
+# converting strings to array of strings 
+# using the strsplit function
+string1_split = strsplit(string1, "")[[1]]
+
+string2_split = strsplit(string2, "")[[1]]
+
+
+for (i in 1:min_length){
+	if (string1_split[i] != string2_split[i]){
+		count = count + 1}}
+
+cat(count)
 
 # I love team-lander
